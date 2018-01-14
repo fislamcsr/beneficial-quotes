ae04514ecef9557e389bd27445063d0e6cc23d99
Fakhrul Islam <fislamcsr@gmail.com>
2018-01-15 03:48:13 +0600

    Update IT-Quotes.js

--- IT-Quotes.js
+++ IT-Quotes.js
@@ -1,14 +1,14 @@
 var quotes = {
-  {"quote":"Talk is cheap. Show me the code.",
+  "1": {"quote":"Talk is cheap. Show me the code.",
   "author":"Linus Torvalds"},
-  {"quote":"Walking on water and developing software from a specification are easy if both are frozen.",
+  "2": {"quote":"Walking on water and developing software from a specification are easy if both are frozen.",
   "author":"Edward Berard"},
-  {"quote":"How you look at it is pretty much how you'll see it",
+  "3": {"quote":"How you look at it is pretty much how you'll see it",
   "author":"Rasheed Ogunlaru"},
-  {"quote":"I'm not a great programmer; I'm just a good programmer with great habits.",
+  "4": {"quote":"I'm not a great programmer; I'm just a good programmer with great habits.",
   "author":"Kent Beck"},
-  {"quote":"Truth can only be found in one place: the code.",
+  "5": {"quote":"Truth can only be found in one place: the code.",
   "author":"Robert C. Martin, Clean Code: A Handbook of Agile Software Craftsmanship"},
-  {"quote":"A language that doesn't affect the way you think about programming is not worth knowing.",
+  "6": {"quote":"A language that doesn't affect the way you think about programming is not worth knowing.",
   "author":"Alan J. Perlis"}
 };
