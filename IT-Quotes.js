6ce8a7615a6c9c9e8ead926b33b8271f8a1d2a62
Fakhrul Islam <fislamcsr@gmail.com>
2018-01-15 03:41:32 +0600

    Update IT-Quotes.js

--- IT-Quotes.js
+++ IT-Quotes.js
@@ -1,4 +1,4 @@
-var quotes = [
+var quotes = {
   {"quote":"Talk is cheap. Show me the code.",
   "author":"Linus Torvalds"},
   {"quote":"Walking on water and developing software from a specification are easy if both are frozen.",
@@ -11,4 +11,4 @@ var quotes = [
   "author":"Robert C. Martin, Clean Code: A Handbook of Agile Software Craftsmanship"},
   {"quote":"A language that doesn't affect the way you think about programming is not worth knowing.",
   "author":"Alan J. Perlis"}
-];
+};
